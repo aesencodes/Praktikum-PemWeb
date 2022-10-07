59f2a9050208cc6554f4e37d3e109340e4e3d8bc
aesencodes <adjiesuryanugraha1@gmail.com>
2022-10-07 22:56:28 +0700

    Added latihan-percobaan

---
+++ Pertemuan 2/latihan-percobaan/form.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_GET['name'];
+$nonton = $_GET['nonton'];
+$apa = $_GET['ini'];
+$apa = $_GET['itu'];
+$apa = $_GET['anu'];
+
+echo "Nama kamu? " .$name;
+echo "<br/>";
+echo "Apakah kamu sudah menonton video saya? : " .$nonton;
+echo "<br/>";
+echo "Apa Coba?  : " .$apa;
+?>
